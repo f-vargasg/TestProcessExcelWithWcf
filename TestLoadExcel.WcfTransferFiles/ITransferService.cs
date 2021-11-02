e39aec3badf436c173a5f2a6b7023cc3e4d2f6bd
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-11-01 19:41:07 -0600

    Primera version depues de cambios fuertes

---
+++ TestLoadExcel.WcfTransferFiles/ITransferService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestLoadExcel.WcfTransferFiles
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ITransferService
+    {
+
+        [OperationContract]
+        RemoteFileInfo DownloadFile(DownloadRequest request);
+
+        [OperationContract]
+        void UploadFile(RemoteFileInfo request);
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [MessageContract]
+    public class DownloadRequest
+    {
+        [MessageBodyMember]
+        public string FileName;
+    }
+
+    [MessageContract]
+    public class RemoteFileInfo : IDisposable
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string FileName;
+
+        [MessageHeader(MustUnderstand = true)]
+        public long Length;
+
+        [MessageBodyMember(Order = 1)]
+        public System.IO.Stream FileByteStream;
+
+        public void Dispose()
+        {
+            if (FileByteStream != null)
+            {
+                FileByteStream.Close();
+                FileByteStream = null;
+            }
+        }
+    }
+}
---
+++ TestLoadExcel.WcfTransferFiles/TransferService.svc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestLoadExcel.WcfTransferFiles
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class TransferService : ITransferService
+    {
+        public RemoteFileInfo DownloadFile(DownloadRequest request)
+        {
+            RemoteFileInfo result = new RemoteFileInfo();
+            try
+            {
+                string filePath = Path.Combine(@"c:\Uploadfiles", request.FileName);
+                FileInfo fileInfo = new FileInfo(filePath);
+
+                // check if exists
+                if (!fileInfo.Exists)
+                    throw new FileNotFoundException("File not found",
+                                                            request.FileName);
+
+                // open stream
+                FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                // return result 
+                result.FileName = request.FileName;
+                result.Length = fileInfo.Length;
+                result.FileByteStream = stream;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+
+        public void UploadFile(RemoteFileInfo request)
+        {
+            FileStream targetStream = null;
+            Stream sourceStream = request.FileByteStream;
+
+            string uploadFolder = @"C:\upload\";
+
+            string filePath = Path.Combine(uploadFolder, request.FileName);
+
+            using (targetStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, 
+                                                 FileShare.None))
+            {
+                //read from the input stream in 65000 byte chunks
+
+                const int bufferLen = 65000;
+                byte[] buffer = new byte[bufferLen];
+                int count = 0;
+                while ((count = sourceStream.Read(buffer, 0, bufferLen)) > 0)
+                {
+                    // save to output stream
+                    targetStream.Write(buffer, 0, count);
+                }
+                targetStream.Close();
+                sourceStream.Close();
+            }
+        }
+    }
+}
