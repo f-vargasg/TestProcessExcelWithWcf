5bb98177fc937f8f483d75539ecdbccf4517169c
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-11-05 09:36:10 -0600

    Cambio en la forma 5 noviembre 2021

--- WinTestService/FrmPrincipal.Designer.cs
+++ WinTestService/FrmPrincipal.Designer.cs
@@ -45,12 +45,13 @@
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tlsStrExit,
             this.toolStripButton2});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(919, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(919, 27);
             this.toolStrip1.TabIndex = 7;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -60,7 +61,7 @@
             this.tlsStrExit.Image = ((System.Drawing.Image)(resources.GetObject("tlsStrExit.Image")));
             this.tlsStrExit.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tlsStrExit.Name = "tlsStrExit";
-            this.tlsStrExit.Size = new System.Drawing.Size(23, 22);
+            this.tlsStrExit.Size = new System.Drawing.Size(29, 24);
             this.tlsStrExit.Text = "toolStripButton1";
             this.tlsStrExit.ToolTipText = "Salir";
             this.tlsStrExit.Click += new System.EventHandler(this.tlsStrExit_Click);
@@ -71,7 +72,7 @@
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 24);
             this.toolStripButton2.Text = "toolStripButton2";
             this.toolStripButton2.ToolTipText = "Test";
             // 
@@ -80,19 +81,19 @@
             this.tblServices.Controls.Add(this.tbPgPrimVersion);
             this.tblServices.Controls.Add(this.tbPgSegVersion);
             this.tblServices.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tblServices.Location = new System.Drawing.Point(0, 25);
+            this.tblServices.Location = new System.Drawing.Point(0, 27);
             this.tblServices.Name = "tblServices";
             this.tblServices.SelectedIndex = 0;
-            this.tblServices.Size = new System.Drawing.Size(919, 454);
+            this.tblServices.Size = new System.Drawing.Size(919, 452);
             this.tblServices.TabIndex = 8;
             // 
             // tbPgPrimVersion
             // 
             this.tbPgPrimVersion.Controls.Add(this.ucPrimVersTransfFile);
-            this.tbPgPrimVersion.Location = new System.Drawing.Point(4, 25);
+            this.tbPgPrimVersion.Location = new System.Drawing.Point(4, 29);
             this.tbPgPrimVersion.Name = "tbPgPrimVersion";
             this.tbPgPrimVersion.Padding = new System.Windows.Forms.Padding(3);
-            this.tbPgPrimVersion.Size = new System.Drawing.Size(911, 425);
+            this.tbPgPrimVersion.Size = new System.Drawing.Size(911, 419);
             this.tbPgPrimVersion.TabIndex = 0;
             this.tbPgPrimVersion.Text = "Primera Versión";
             this.tbPgPrimVersion.UseVisualStyleBackColor = true;
@@ -100,18 +101,18 @@
             // ucPrimVersTransfFile
             // 
             this.ucPrimVersTransfFile.Location = new System.Drawing.Point(0, 0);
-            this.ucPrimVersTransfFile.Margin = new System.Windows.Forms.Padding(4);
+            this.ucPrimVersTransfFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ucPrimVersTransfFile.Name = "ucPrimVersTransfFile";
-            this.ucPrimVersTransfFile.Size = new System.Drawing.Size(776, 289);
+            this.ucPrimVersTransfFile.Size = new System.Drawing.Size(856, 289);
             this.ucPrimVersTransfFile.TabIndex = 0;
             // 
             // tbPgSegVersion
             // 
             this.tbPgSegVersion.Controls.Add(this.ucSegVersTransfFile);
-            this.tbPgSegVersion.Location = new System.Drawing.Point(4, 25);
+            this.tbPgSegVersion.Location = new System.Drawing.Point(4, 29);
             this.tbPgSegVersion.Name = "tbPgSegVersion";
             this.tbPgSegVersion.Padding = new System.Windows.Forms.Padding(3);
-            this.tbPgSegVersion.Size = new System.Drawing.Size(911, 425);
+            this.tbPgSegVersion.Size = new System.Drawing.Size(911, 419);
             this.tbPgSegVersion.TabIndex = 1;
             this.tbPgSegVersion.Text = "Segunda Versión";
             this.tbPgSegVersion.UseVisualStyleBackColor = true;
@@ -126,7 +127,7 @@
             // 
             // FrmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(919, 479);
             this.Controls.Add(this.tblServices);
